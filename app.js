c076f6ff04bb20393a5844b390dd960c62553431
Hack Reactor Students <github@hackreactor.com>
2014-03-31 15:21:09 -0700

    angular presentPosts controller and search by slugs

--- app.js
+++ app.js
@@ -1,30 +1,44 @@
-var query = 'http://data.nasa.gov/api/get_tag_datasets/?slug=apollo&slug=imagery&count=1';
-var slugs = {};
-$(document).ready(function(){
-  var getData = function(query){
+var query = 'http://data.nasa.gov/api/get_recent_datasets?count=100';
+
+var app = angular.module("spaceLinks", []);
+app.controller('presentPosts', function($scope){
+
+  $scope.ids = [];
+  $scope.slugs = {'':$scope.ids};
+  $scope.dataPosts = {};
+
+  $scope.getData = function(query){
     $.ajax({
       url: query,
       type: 'GET',
       dataType: 'jsonp',
       success: function(data){
-        filterData(data.posts['0']);
+        filterData(data.posts);
       }
     });
   };
 
-  var filterData = function(post){
+  var filterData = function(posts){
+    for(var i = 0; i < posts.length; i++){
+      filterPost(posts[i]);
+    }
+  }
+
+  var filterPost = function(post){
     var resultsObj = {};
     resultsObj.tags = [];
     var recursiveFilter = function(info){
       for(var point in info){
         if(info[point] !== ''){
           if(typeof info[point] !== 'object' && info[point] !== ''){
-            if(point === 'content' || point === 'date' || point === 'modified' || point === 'id'){
+            if(point === 'content' || point === 'date' || point === 'modified'){
               resultsObj[point] = info[point];
             } else if (point === 'title' && resultsObj.title===undefined){
               resultsObj.title = info[point];
             } else if(point === 'slug'){
               resultsObj.tags.push(info[point]);
+            } else if(point === 'id'){
+              resultsObj.srcId = info[point];
             }
           } else if(point === 'more_info_link') {
             resultsObj.source = info[point][0];
@@ -34,39 +48,59 @@ $(document).ready(function(){
         }
       }
     };
+    if($scope.ids.length === 0){
+      resultsObj.id = 1;
+    } else {
+      resultsObj.id = $scope.ids[$scope.ids.length-1] + 1;
+    }
+    $scope.ids.push(resultsObj.id);
+    $scope.dataPosts[resultsObj.id] = resultsObj;
     recursiveFilter(post);
     slugBuilder(resultsObj);
-    displayData(resultsObj);
+    displayPost(resultsObj);
   };
 
   var slugBuilder = function(results){
     for(var i = 0; i < results.tags.length; i++){
-      if(slugs[results.tags[i]] === undefined){
-        slugs[results.tags[i]] = [];
+      if($scope.slugs[results.tags[i]] === undefined){
+        $scope.slugs[results.tags[i]] = [];
       }
-      slugs[results.tags[i]].push(results.id);
+      $scope.slugs[results.tags[i]].push(results.id);
     }
-    console.log(slugs);
   }
-  var displayData = function(results){
+  var displayPost = function(results){
+    var $post = angular.element('<div></div>');
     var $el;
     for(var item in results){
       if(item === 'content'){
         $el = results[item];
       } else if (item === 'title'){
-        $el = $('<h3 class="title"></h3>').text(results[item]);
+        $el = angular.element('<h3 class="title"></h3>').text(results[item]);
       } else if (item === 'source'){
-        $el = $('<a href='+results[item]+'></a>');
+        $el = angular.element('<a href='+results[item]+'></a>');
         $el.text(results[item]);
-      } else if (item !== 'tags'){
-        $el = $('<p></p>').text(item+': '+results[item]);
+      } else if (item !== 'tags' && item !== 'srcId' && item !== 'id'){
+        $el = angular.element('<p></p>').text(item+': '+results[item]);
       }
-      $('.container').append($el);
+      $post.append($el);
+    }
+    angular.element('.container').append($post);
+  };
+
+  angular.element('button').on('click', function(){
+    $scope.getData(query);
+  });
+  
+  $scope.onSearch = function(){
+    var tag = $scope.slugs[$scope.key];
+    console.log($scope.key);
+    console.log(tag);
+    angular.element('.container').empty();
+    if(!!tag){
+      for(var i = 0; i < tag.length; i++){
+        displayPost($scope.dataPosts[i]);
+      }
     }
   };
 
-
-  $('button').on('click', function(){
-    getData(query);
-  });
 });
