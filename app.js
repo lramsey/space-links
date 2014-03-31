f027f590eb82c5855e6633002015008e515eb6af
Hack Reactor Students <github@hackreactor.com>
2014-03-31 12:20:26 -0700

    create dataFilter and displayData functions

--- app.js
+++ app.js
@@ -2,14 +2,59 @@ var query = 'http://data.nasa.gov/api/get_tag_datasets/?slug=apollo&slug=imagery
 
 $(document).ready(function(){
   var getData = function(query){
-    .ajax({
+    $.ajax({
       url: query,
       type: 'GET',
       dataType: 'jsonp',
       success: function(data){
-        console.log(data);
+        filterData(data.posts['0']);
       }
+    });
+  };
+
+  var filterData = function(post){
+    var resultsObj = {};
+    resultsObj.tags = [];
+    var recursiveFilter = function(info){
+      for(var point in info){
+        if(info[point] !== ''){
+          if(typeof info[point] !== 'object' && info[point] !== ''){
+            if(point === 'content' || point === 'date' || point === 'modified'){
+              resultsObj[point] = info[point];
+            } else if (point === 'title' && resultsObj.title===undefined){
+              resultsObj.title = info[point];
+            } else if(point === 'slug'){
+              resultsObj.tags.push(info[point]);
+            }
+          } else if(point === 'more_info_link') {
+            resultsObj.source = info[point][0];
+          } else {
+            recursiveFilter(info[point]);
+          }
+        }
+      }
+    };
+    recursiveFilter(post);
+    displayData(resultsObj);
+  };
+
+  var displayData = function(results){
+    var $el;
+    for(var item in results){
+      if(item === 'content'){
+        $el = results[item];
+      } else if (item === 'source'){
+        $el = $('<a href='+results[item]+'></a>');
+        $el.text(results[item]);
+      } else if (item !== 'tags'){
+        $el = $('<p></p>').text(item+': '+results[item]);
+      }
+      $('.container').append($el);
     }
-  }
-  $('button').on('click', getData(query)
+  };
+
+
+  $('button').on('click', function(){
+    getData(query);
+  });
 });
